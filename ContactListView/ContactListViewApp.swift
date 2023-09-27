5fa5c6eac4c82029278e4f5e1cd39b9f2e013453
ChauveSouris <desseobscurite@gmail.com>
2023-09-28 00:42:09 +0500

    Initial Commit

---
+++ ContactListView/ContactListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListViewApp.swift
+//  ContactListView
+//
+//  Created by Елизавета Медведева on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
