74a58df0b0797be6fc7d977a7f9bfba8323959cc
ChauveSouris <desseobscurite@gmail.com>
2023-09-28 23:19:15 +0500

    Incorected Model

--- ContactListView/ViewModel.swift
+++ ContactListView/ViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct User {
+struct Person {
     
     let name: String
     let surname: String
@@ -16,5 +16,28 @@ struct User {
     
     var fullName: String {
         "\(name) \(surname)"
+    }
+    
+    func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let numbers = DataManager.shared.numbers.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        
+        let minimumValue = emails.count
+        
+        for index in (0...minimumValue) {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                number: numbers[index],
+                email: emails[index]
+            )
+        }
+        
+        return persons
     }
 }
